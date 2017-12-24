6ef38d8a08dfa890d4cb2fa5cd68b4e83ad43155
fengziyu <fengziyu@apexsoft.com.cn>
2017-12-24 22:56:12 +0800

    添加远程命令管理类

--- Camera/managers.py
+++ Camera/managers.py
@@ -1,7 +1,9 @@
 #encoding=utf-8
 import cv2
 import numpy
-import time
+import socket
+import json
+import threading
 
 class CameraManager(object):
     #相机管理类
@@ -14,12 +16,19 @@ class CameraManager(object):
         self._windowManager = windowManager
         self._isMirror = isMirror
         self._capture = capture
+        #当前画面
         self._frame = None
+        #截图文件
         self._imageFilename = None
+        #录像文件
         self._videoFilename = None
+        #录像编码
         self._videoEncoding = None
+        #视频写入工具
         self._videoWriter = None
+        #是否开启延时
         self._isShow = True
+        #是否开启工作
         self._isWorking = True
     def getFrame(self):
         #返回当前的帧
@@ -36,8 +45,10 @@ class CameraManager(object):
             _,self._frame = self._capture.read()
     def processFrame(self):
         #处理每一帧图像的函数,其中可以运行包括写入视频,写入图片,显示视频的钩子函数
-        if self._windowManager is not None:
+        if self._windowManager is not None and self._isWorking:
+            #是否工作
             if self._isShow:
+                #是否开启显示
                 if self._isMirror:
                     #是否开启镜像
                     mirrorFrame = numpy.fliplr(self._frame).copy()
@@ -89,15 +100,18 @@ class CameraManager(object):
         #工作控制
         return self._isWorking
 class WindowManager(object):
-    #窗口管理类
-    def __init__(self,windowName,keypressCallback = None):
+    #视频控制类
+    def __init__(self,windowName,keypressCallback = None,commandCallback=None,commandManager=None):
         '''
         :param windowName:窗口名称 
         :param keypressCallback: 按钮回调函数
         '''
         self.keypressCallback = keypressCallback
+        self.commandCallback = commandCallback
         self.windowName = windowName
         self._isWindowExist = False
+        self.commandManager = commandManager
+        self.commandManager.startWorking()
     def isWindowExist(self):
         #是否存在窗口
         return self._isWindowExist
@@ -112,10 +126,50 @@ class WindowManager(object):
         #销毁窗口
         cv2.destroyWindow(self.windowName)
         self._isWindowExist = False
-    def processEvents(self):
-        #事件处理函数
+    def stopWorking(self):
+        cv2.destroyWindow(self.windowName)
+        self._isWindowExist = False
+        self.commandManager.stopWorking()
+    def processKeyEvents(self):
+        #按键事件处理函数
         keycode = cv2.waitKey(1)
         if self.keypressCallback is not None and keycode != -1:
             keycode &= 0xFF
             self.keypressCallback(keycode)
+    def processCommandEvents(self):
+        #命令时间处理函数
+        command = self.commandManager.getCommand()
+        if self.commandCallback is not None and command is not None:
+            self.commandCallback(command)
+class CommandManager(object):
+    def __init__(self,ip,port):
+        self.ip = ip
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.sock.bind((self.ip,self.port))
+        self.command = None
+        self._isWorking = True
+        self._task = None
+        self.startWorking()
+    def _listenCommand(self):
+        #开启侦听
+        while self._isWorking:
+            data, addr = self.sock.recvfrom(1024 * 1024)
+            jsondata = json.loads(data.decode())
+            self.command = jsondata["command"]
+            print(self.command)
+    def startWorking(self):
+        #开始工作
+        self._task = threading.Thread(target=self._listenCommand)
+        self._task.start()
+    def stopWorking(self):
+        #结束工作
+        self._isWorking = False
+        self.sock.close()
+        self._task = None
+    def getCommand(self):
+        #获得发送的命令
+        result = self.command
+        self.command = None
+        return result
 

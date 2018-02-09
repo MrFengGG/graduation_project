1843ee0aae301b97b44ba9c6140a3c50f27105f7
fengziyu <fengziyu@apexsoft.com.cn>
2018-02-09 22:05:44 +0800

    添加配置文件

--- camera/settings.py
+++ camera/settings.py
@@ -1,2 +1,25 @@
+import sys
+#照片保存文件夹
 SCREENSHOT_DIR = "screenshots/"
+#视频保存文件夹
 VIDEO_DIR = "videos/"
+#预警信息文件夹
+WARN_DIR = "warn/"
+#日志文件夹
+LOG_DIR = "log/"
+#日志文件
+LOG_FILE = "camera.log"
+#命令监听ip
+commandIp = ""
+#命令监听端口
+commandPort = 9998
+#图像发送IP
+imageIp = "127.0.0.1"
+#图像发送端口
+imagePort = 9999
+#日志输出格式
+logFormatter = "%(asctime)s %(levelname)-8s: %(message)s"
+#日志输出名称
+logName = 'cameraLogger'
+#日志文件路径
+logFile = LOG_DIR + LOG_FILE
